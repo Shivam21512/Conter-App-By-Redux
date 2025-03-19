8e9ed31169bebe4b7de366e45b85e2176e2ff6a2
Shivam21512 <ugaleshivam73@gmail.com>
2025-03-19 10:43:17 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import Counter from "./Components/Counter"
+
+const App = () => {
+  return (
+    <div className="w-full h-screen flex bg-[#344151] items-center justify-center flex-col gap-10">
+      <Counter/>
+
+    </div>
+  )
+};
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import Counter from "./Components/Counter";
+
+// const App = () => {
+//   return (
+//     <div className="w-full h-screen flex bg-[#344151] items-center justify-center flex-col gap-10">
+//       <Counter />
+//     </div>
+//   )
+// };
+
+// export default App;
