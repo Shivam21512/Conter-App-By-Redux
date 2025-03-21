990897d32993cb3895c862d126f982d00f0079e9
Shivam Ugale <141569702+Shivam21512@users.noreply.github.com>
2025-03-21 18:35:13 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,29 +10,3 @@ const App = () => {
 };
 
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import Counter from "./Components/Counter";
-
-// const App = () => {
-//   return (
-//     <div className="w-full h-screen flex bg-[#344151] items-center justify-center flex-col gap-10">
-//       <Counter />
-//     </div>
-//   )
-// };
-
-// export default App;
